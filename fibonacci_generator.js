bcd1e83cfafcb54f9c7b2bdbab390c43134da648
Akash RP <95461248+Akashrp512@users.noreply.github.com>
2022-08-29 18:38:38 +0530

    Update fibonacci_generator.js

--- fibonacci_generator.js
+++ fibonacci_generator.js
@@ -1,3 +1,4 @@
+//fibonacci generator using for loop
 function fibonacciGenerator(n);{
 var output=[];
 if(n===1){
@@ -16,3 +17,23 @@ else{
 return output;
 }
 fibonacciGenerator(5); //vary the n value
+
+//fibonacci sequence using if-else condition
+var output=[];
+if(n===1){
+    output[0];
+}
+else if(n===2){
+    output=[0,1]
+}
+else{
+    output=[0,1];
+    output.push(output[output.length-2]+output[output.length-1]); //[0,1,1]
+    if(n===output.length){
+
+    }
+    else{
+        output.push(output[output.length-2]+output[output.length-1]);
+    }
+}
+return output;
