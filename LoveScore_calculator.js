9af0eff17e80e1fd0e1fef345c964a485fccc349
Akash RP <95461248+Akashrp512@users.noreply.github.com>
2022-08-29 18:26:58 +0530

    Create LoveScore_calculator.js

---
+++ LoveScore_calculator.js
@@ -0,0 +1,28 @@
+//LOVE SCORE CALCULATOR
+prompt("what is your name");
+prompt("What is their name");
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+if(loveScore>70){
+    alert("Your love Score is "+loveScore+"%"+"You are like Romeo and Juliet");
+}
+else{
+    alert("Your love Score is "+loveScore+"%");
+}
+
+//Adding Conditional Statement
+prompt("what is your name");
+prompt("What is their name");
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+if(loveScore>70){
+    alert("Your love Score is "+loveScore+"%"+" You are like Romeo and Juliet");
+}
+if(loveScore>30 && loveScore<=70)
+{
+    alert("Your love Score is "+loveScore+"%"+"you are like tajmahal");
+}
+ if(loveScore<30)
+ {
+ alert("Your love Score is "+loveScore+"%"+"you go together like oil and water");   
+}
