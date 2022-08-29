9a8aa82f0da7b674584ef4bb8ac6e0fc25d40d58
Akash RP <95461248+Akashrp512@users.noreply.github.com>
2022-08-29 18:39:34 +0530

    Create numberOfBottle_lyrics.js

---
+++ numberOfBottle_lyrics.js
@@ -0,0 +1,13 @@
+//No of bottle Decrementing Lyrics using While Loop
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
