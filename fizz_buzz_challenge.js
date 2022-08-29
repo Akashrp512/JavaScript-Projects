91d90e781d7f38534969af81bdfb0de62ba6a74f
Akash RP <95461248+Akashrp512@users.noreply.github.com>
2022-08-29 18:28:18 +0530

    Create fizz_buzz_challenge.js

---
+++ fizz_buzz_challenge.js
@@ -0,0 +1,22 @@
+//Fizz buzz challenge
+var output=[];
+var count=1;
+function fizzbuzz() {
+output.push(count); 
+ count++;
+ if(count% 3 ===0 && count%5 ===0){
+  output.push("fizzbuzz");
+ }
+ else if(count%3 === 0){
+  output.push("fizz");
+ }
+ else if(count%5 === 0)
+ {
+  output.push("buzz");
+ }
+ else{
+  output.push(count)
+ }
+ count++;
+ console.log(output);
+}
